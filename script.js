f07e7f91d44ebac9b92457c97614e3cdefb6fe50
Dharmander Yadav <imDharmanderyadav@users.noreply.github.com>
2024-01-23 00:30:00 +0530

    initial commit
    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+let login =  document.getElementById("login");
+
+function redirectToAttendance() {
+    var password = document.getElementById("pass").value;
+    
+    // Replace 'YOUR_SERVER_URL' with the actual server URL where you handle password validation
+    var serverUrl = 'http://gnioterp.com:150';
+    
+    // http://gnioterp.com:150/StudentAcademicReportMgmt.aspx?StuId=73991&SessionId=1022&ClassId=31&SemId=61
+
+    // Redirect to the attendance page with the password as a parameter
+    window.location.href = serverUrl + '/StudentAcademicReportMgmt.aspx?StuId=' + encodeURIComponent(password)+'&SessionId=1022&ClassId=31&SemId=61';
+}
+
+login.addEventListener("click", (e)=>{
+    e.preventDefault();
+    redirectToAttendance();
+});
